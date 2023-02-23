a12e65a62dd08b84d12e321826d80c19fc26547c
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-02-24 11:57:40 +1300

    initialize

---
+++ cs-howick/cs-howick/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace CSHowick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string storyName = "Jose";
+            int storyAge;
+            storyAge = 33;
+
+            Console.WriteLine("Hello Jo");
+            Console.WriteLine("this is my Boat");
+            Console.WriteLine("         /|      ");
+            Console.WriteLine("        / |      ");
+            Console.WriteLine("       /  |      ");
+            Console.WriteLine("      /   |      ");
+            Console.WriteLine("     /    |      ");
+            Console.WriteLine("    /     |      ");
+            Console.WriteLine("   /______|      ");
+            Console.WriteLine("    /_____|      ");
+            Console.WriteLine("    /_____|      ");
+            Console.WriteLine("  _______||______");
+            Console.WriteLine("  |             /");
+            Console.WriteLine("  |____________/");
+            Console.WriteLine("\" In this boat there's a man named " + storyName);
+            Console.WriteLine("   He is " + storyAge, ", He is kind and very obedient");
+            Console.WriteLine(storyName + ", Likes riding his boat and fishing");
+            Console.WriteLine("One day, " + storyName + "just sailed away, and never came back \" ");
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
